88ec391f55fe59d9a125cdd437e8efcb0d22e68a
Arupananda Pal <arupananda.pal@bipros.com>
2024-12-30 16:27:01 +0530

    Initial Commit

---
+++ src/main/java/com/arup/FbBusinessSdkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.arup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbBusinessSdkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbBusinessSdkSpringbootApplication.class, args);
+	}
+
+}
